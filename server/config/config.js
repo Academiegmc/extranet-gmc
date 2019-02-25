bf6728d632da13def7780e5ca006a1eacf754c04
Academie-GMC <service.informatique@academie-gmc.com>
2019-02-25 17:59:23 +0100

    Adding nodemailer credentials

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,18 @@
+const {
+  NODEMAILER_USER_EMAIL,
+  NODEMAILER_USER_PASSWORD,
+  NODEMAILER_SERVICE,
+  NODEMAILER_SECURE,
+  NODEMAILER_TLS
+} = process.env;
 module.exports = {
   port: 5000,
   mail: {
-    auth: { user: "test.mail@academie-gmc.com", pass: "testMail" },
-    service: "gmail",
-    secure: false,
+    auth: { user: NODEMAILER_USER_EMAIL, pass: NODEMAILER_USER_PASSWORD },
+    service: NODEMAILER_SERVICE,
+    secure: NODEMAILER_SECURE,
     tls: {
-      rejectUnauthorized: false
+      rejectUnauthorized: NODEMAILER_TLS
     },
     to: "quentin.kabasele@academie-gmc.com",
     subject: "Candidature",
