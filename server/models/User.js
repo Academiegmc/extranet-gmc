bc6b597493c4893dc27c9b62b14966de8f728116
Academie-GMC <service.informatique@academie-gmc.com>
2019-02-25 17:50:18 +0100

    login test 4

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true, select: false },
+  password: { type: String, required: true },
   admin: { type: Boolean, default: false },
   status: { type: Number, required: true },
   experiences: { type: [{}] },
