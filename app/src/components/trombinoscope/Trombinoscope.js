3358cc4b1d75b63f9a471e2709d11a7fcb1c25af
Academie-GMC <service.informatique@academie-gmc.com>
2019-04-02 17:55:25 +0200

    Frontend: Trombinoscope updated

--- app/src/components/trombinoscope/Trombinoscope.js
+++ app/src/components/trombinoscope/Trombinoscope.js
@@ -1,48 +1,97 @@
 import React, { Component, PureComponent } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import data from "../../mock/trombinoscope.json";
+import "./Trombinoscope.css";
 import { getAllUsers } from "../../actions/usersAction";
 import ReturnButton from "../layout/ReturnButton";
 class Trombinoscope extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      data: {}
     };
   }
-  componentDidUpdate(prevProps, prevState) {
-    // console.log(this.props.users.users.data);
-    if (this.props.users.users.data) {
-      this.setState({ users: this.props.users.users.data });
-    }
-  }
+  componentDidUpdate(prevProps, prevState) {}
   componentDidMount() {
-    this.props.getAllUsers();
+    this.setState({ data });
   }
   render() {
     let status;
-    const { users } = this.state;
-    const allUsers = users.map((user, index) => {
-      if (user.status === 0) status = "élève";
-      if (user.status === 1) status = "Ancien élève";
-      if (user.status === 2) status = "Professeur";
-      if (user.status === 3) status = "Administrateur";
-      return (
-        <div key={index}>
-          <h6>{user.name}</h6>
-          <p>{user.email}</p>
-          <p>{status}</p>
-          <hr />
-        </div>
-      );
-    });
+    console.log(this.state.data);
+    const { administration, pedagogie } = this.state.data;
+    const allUsers =
+      administration !== undefined
+        ? administration.map((user, index) => {
+            if (user.status === 0) status = "élève";
+            if (user.status === 1) status = "Ancien élève";
+            if (user.status === 2) status = "Professeur";
+            if (user.status === 3) status = "Administrateur";
+            return (
+              <div
+                className="animated-card w-auto d-flex flex-column text-center m-2 align-items-center"
+                key={index}
+              >
+                <img
+                  className="portrait border"
+                  src={require(`../../assets/${user.image}`)}
+                  alt="User proile pic"
+                />
+                <h6 className="title">{user.name}</h6>
+                <p className="text">{user.fonction}</p>
+                <p className="text">{status}</p>
+                <hr />
+              </div>
+            );
+          })
+        : null;
+    const allPedago =
+      pedagogie !== undefined
+        ? pedagogie.map((user, index) => {
+            if (user.status === 0) status = "élève";
+            if (user.status === 1) status = "Ancien élève";
+            if (user.status === 2) status = "Professeur";
+            if (user.status === 3) status = "Administrateur";
+            return (
+              <div
+                className="w-auto d-flex flex-column text-center m-2 align-items-center"
+                key={index}
+              >
+                <img
+                  className="portrait border"
+                  src={require(`../../assets/${user.image}`)}
+                  alt="User proile pic"
+                />
+                <h6 className="title">{user.name}</h6>
+                <p className="text text-wrap">{user.fonction}</p>
+                <p className="text">{status}</p>
+                <hr />
+              </div>
+            );
+          })
+        : null;
     return (
-      <div className="container">
-        <div className="flex flex-row flex-center">
+      <div className="container-fluid">
+        <div className="d-flex flex-row flex-center">
           <ReturnButton history={this.props.history} />
           <h1>Trombinoscope</h1>
         </div>
-        <div className="flex flex-column">{allUsers}</div>
+        <h2 className="text-center">Administration</h2>
+        <div
+          id="section-one"
+          className="d-sm-flex flex-sm-column flex-md-row flex-wrap"
+        >
+          {allUsers}
+        </div>
+        <hr />
+        <h2 className="text-center">Pédagogie</h2>
+        <div
+          id="section-two"
+          className="d-sm-flex flex-sm-column flex-md-row flex-wrap"
+        >
+          {allPedago}
+        </div>
       </div>
     );
   }
